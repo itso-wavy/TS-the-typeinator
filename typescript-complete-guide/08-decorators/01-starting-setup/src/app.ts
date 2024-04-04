6316fa5c5a1dbfb3c2ac944ee6a5e04a0c4c5c38
itso-wavy <you.meet.vvavy@gmail.com>
2024-04-04 23:55:53 +0900

    .

---
+++ typescript-complete-guide/08-decorators/01-starting-setup/src/app.ts
@@ -0,0 +1,48 @@
+// "strictNullChecks": 엄격한 null 검사
+
+// "strictNullChecks": true의 경우
+const $button = document.querySelector('button');
+// const $button: HTMLButtonElement | null
+
+const $button2 = document.querySelector('button')!;
+// $button2: HTMLButtonElement: ! 연산자로 null 가능성 제거
+$button2.addEventListener('click', () => {
+  console.log('clicked!');
+});
+
+// "strictNullChecks": false의 경우
+const $button3 = document.querySelector('button')!;
+
+if ($button3) {
+  function clickHandler(message: string) {
+    console.log('clicked!' + message);
+  }
+
+  $button3.addEventListener('click', clickHandler.bind(this, 'hello'));
+}
+
+// "noImplicitThis": true,
+class MyClass {
+  myMethod() {
+    console.log(this);
+  }
+}
+
+let obj = {
+  aMethod: new MyClass().myMethod,
+};
+
+obj.aMethod(); // 현재 코드 블록 내에서 this의 참좃값을 파악하기 어려운 경우 경고함
+
+// "noImplicitReturns": true
+function implicitTest(num: number) {
+  if (num > 0) return num;
+  if (num <= 0) return;
+  return; // 이 부분을 없애면 경고
+}
+
+const a = implicitTest(1);
+const b = implicitTest(0);
+const c = implicitTest(-1);
+
+console.log(a, b, c);
--- typescript-complete-guide/15-node/05-finished/src/app.ts
+++ typescript-complete-guide/15-node/05-finished/src/app.ts
@@ -17,21 +17,3 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 });
 
 app.listen(3000);
-
-// `rfce`
-// React.FunctionComponent: JSX를 반환하는 함수
-// return문을 없애면 에러
-// React.ClassicComponent
-
-/* 
-useRef<T>(T | null)
-React에서 ref가 초기화되는 것은 해당 ref 객체가 해당하는 DOM 요소와 연결되는 것을 의미합니다.
-일반적으로 ref를 사용하여 DOM 요소에 접근하려면 해당 요소가 렌더링될 때 생성되어야 합니다. React는 컴포넌트가 렌더링될 때 ref를 참조하는 DOM 요소를 찾고 ref 객체의 current 속성에 할당하여 초기화합니다.
-
-const formSubmitHandler = (e: React.FormEvent) => {};
-
----
-
-body-parser: 
-// 요청의 body을 파싱하고 Express 애플리케이션에 주입하는 역할, JSON, URL-encoded, 그리고 멀티파트 형식의 데이터까지 파싱, 처리할 수 있음
-*/
