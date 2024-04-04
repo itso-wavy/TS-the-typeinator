ded54893ba6f899fd87a02d0d4cf812516217297
wavy_ <you.meet.vvavy@gmail.com>
2024-04-04 23:57:16 +0900

    Delete typescript-complete-guide/08-decorators directory

--- typescript-complete-guide/08-decorators/01-starting-setup/src/app.js
+++
@@ -1,8 +0,0 @@
-console.log('Your code goes here...');
-var Role;
-(function (Role) {
-    Role[Role["a"] = 0] = "a";
-    Role[Role["b"] = 1] = "b";
-    Role[Role["c"] = 2] = "c";
-})(Role || (Role = {}));
-console.log('sum: ', Role.a + Role.b);
